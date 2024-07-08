986e58244675620f814174c6911b989df90d0f97
alxgitwe <el.najib15@gmail.com>
2024-07-08 16:00:30 -0700

    m

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,56 +1,16 @@
-#!/usr/bin/env bash
-# Automate the task to create a custom HTT header response with Puppet
+# Use Puppet to automate the task of creating a custom HTTP header response
 
-exec { 'nginx install':
-path    => '/usr/bin',
-command => 'sudo apt update && sudo apt-get -y install nginx',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-exec { 'Make directories':
-path    => '/usr/bin',
-command => 'sudo mkdir -p /var/www/html && sudo chown -R "$USER":"$USER" /var/www/html',
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# exec { 'Make file':
-# path    => '/usr/bin',
-# command => 'echo "Hello World!" > /var/www/html/index.html'
-# }
-
-file { '/var/www/html/index.html':
-mode    => '0755',
-content => 'Hello World!',
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file {'/var/www/html/error404.html':
-mode    => '0755',
-content => "Ceci n'est pas une page",
-}
-
-exec { 'block server':
-path    => '/usr/bin',
-command => 'echo "
-server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-
-	add_header X-Served-By \$hostname;
-
-	error_page 404 /error404.html;
-	location = /error404.html {
-		root /var/www/html;
-		internal;
-	}
-
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-}" | sudo tee /etc/nginx/sites-available/default'
-}
-
-exec { 'Restart nginx':
-path    => '/usr/bin',
-command => 'sudo service nginx restart',
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
