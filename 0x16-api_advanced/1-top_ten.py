3e053a465debc5884f22c9ec9170941edad9b5fc
alxgitwe <el.najib15@gmail.com>
2024-09-25 14:41:07 -0700

    m

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-"""Query Reddit API to determine subreddit sub count
-"""
-
+"""Returns first 10 hot posts for a given subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """Request top ten hot posts of subreddit
-    from Reddit API
-    """
-    # set custom user-agent
-    user_agent = '0x16-api_advanced-jmajetich'
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-
-    # custom user-agent avoids request limit
-    headers = {'User-Agent': user_agent}
-
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
-        print('None')
-    else:
-        # load response unit from json
-        data = r.json()['data']
-        # extract list of pages
-        posts = data['children']
-        for post in posts:
-            print(post['data']['title'])
+    """Return first 10 hot posts given a subreddit"""
+    BASE_URL = 'https://api.reddit.com'
+    headers = {
+        "User-Agent": "ChangeMeClient/0.1 by fatima"
+    }
+    params = {
+        "limit": 10
+    }
+    res = requests.get("{}/r/{}/hot".format(BASE_URL, subreddit),
+                       headers=headers, params=params)
+    if res.status_code == 404:
+        return print(None)
+    children = res.json().get('data').get('children')
+    print("\n".join([child.get('data').get('title') for child in children]))
