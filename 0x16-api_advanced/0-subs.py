f095e6de6856555a47ce9c6e0e0e4d05e588e4bf
alxgitwe <el.najib15@gmail.com>
2024-09-25 14:37:21 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Returns subscriber count for a given subreddit"""
+"""
+Contains the number_of_subscribers function
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return number of subscriber given a subreddit"""
-    BASE_URL = 'https://api.reddit.com'
-    headers = {
-        "User-Agent": "ChangeMeClient/0.1 by fatima"
-    }
-    res = requests.get("{}/r/{}/about".format(BASE_URL, subreddit),
-                       headers=headers)
-    if res.status_code == 404:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    return res.json().get('data').get('subscribers')
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
